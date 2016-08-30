09662072936e00520d6771f83c4381c898b98221
Rafal Dittwald <rafal.dittwald@gmail.com>
2016-08-30 18:26:18 -0400

    Simplify how metadata is retrieved

--- src/cheatsheet/core.cljs
+++ src/cheatsheet/core.cljs
@@ -1,45 +1,28 @@
 (ns cheatsheet.core
   (:require [reagent.core :as r]
             [cheatsheet.data :refer [data]]
-            [cheatsheet.eval :refer [eval-code]]
             [cheatsheet.styles :refer [styles-view]]))
 
 (enable-console-print!)
 
-(defonce state (r/atom {}))
-
-(defn get-docs [string]
-  (eval-code (str "(meta #'" string ")")
-             (fn [output]
-               (swap! state assoc string output))))
-
 (defn function-hover-view [item]
-  (if (namespace item)
-    [:div]
-    (let [string (if (namespace item)
-                   (str (namespace item)
-                        "/"
-                        (name item))
-                   (name item))
-          meta (get-in @state [string])
-          _ (when-not meta
-              (get-docs string))]
-      [:div.meta
-       [:div.ns (:ns meta)]
-       [:div.name (:name meta)]
-       [:div.arglists
-        (for [arglist (or (get meta :arglists)
-                          (get-in meta [:top-fn :arglists]))]
-          ^{:key arglist}
-          [:div.arglist (str arglist)])]
-       [:div.doc (:doc meta)]])))
+  (let [meta (meta item)]
+    [:div.meta
+     [:div.ns (:ns meta)]
+     [:div.name (:name meta)]
+     [:div.arglists
+      (for [arglist (or (get meta :arglists)
+                        (get-in meta [:top-fn :arglists]))]
+        ^{:key arglist}
+        [:div.arglist (str arglist)])]
+     [:div.doc (:doc meta)]]))
 
 (defn item-view [item]
   (cond
     (string? item) [:div.item item]
-    (keyword? item) [:div.item
-                     (name item)
-                     [function-hover-view item]]))
+    (var? item) [:div.item
+                 (:name (meta item))
+                 [function-hover-view item]]))
 
 (defn list-view [items]
   [:div.list
--- src/cheatsheet/data.cljs
+++ src/cheatsheet/data.cljs
@@ -3,64 +3,64 @@
 (def data
   {"literals" ["true" "false" "nil"]
 
-   "numbers" {"create"     ["1" "-1.5" :int :long]
-              "arithmetic" [:+ :- :* :/ :max :min :inc :dec]
-              "compare"    [:= :< :> :<= :>=]
-              "random"     [:rand :rand-int]}
+   "numbers" {"create"     ["1" "-1.5" #'int #'long]
+              "arithmetic" [#'+ #'- #'* #'/ #'max #'min #'inc #'dec]
+              "compare"    [#'= #'< #'> #'<= #'>=]
+              "random"     [#'rand #'rand-int]}
 
-   "strings" {"create"     ["\"some-text\"" :str]
-              "use"        [:count
-                            :clojure.string/join
-                            :clojure.string/split
-                            :clojure.string/replace]
-              "test"       [:clojure.string/blank?
-                            :clojure.string/includes?]
+   "strings" {"create"     ["\"some-text\"" #'str]
+              "use"        [#'count
+                            #'clojure.string/join
+                            #'clojure.string/split
+                            #'clojure.string/replace]
+              "test"       [#'clojure.string/blank?
+                            #'clojure.string/includes?]
               "regex"      ["#\"some-patern\""
-                            :re-find
-                            :re-matches]}
+                            #'re-find
+                            #'re-matches]}
 
-   "keywords" {"create"    [":some-keyword"
-                            ":some/keyword"
-                            :keyword]
-               "->str"     [:name
-                            :namespace]}
+   "keywords" {"create"    ["#'some-keyword"
+                            "#'some-ns/some-keyword"
+                            #'keyword]
+               "->str"     [#'name
+                            #'namespace]}
 
    "vectors" {"create" ["[]"
-                        :vec]
+                        #'vec]
               "examine" ["(your-vec idx)"
-                         :get]
-              "change" [:assoc
-                        :assoc-in
-                        :conj]}
+                         #'get]
+              "change" [#'assoc
+                        #'assoc-in
+                        #'conj]}
 
-   "maps" {"create" ["{}" :group-by :hash]
+   "maps" {"create" ["{}" #'group-by #'hash]
            "examine" ["(some-map k)"
                       "(k some-map)"
-                      :get
-                      :get-in
-                      :contains?
-                      :keys
-                      :vals]
-           "change" [:assoc
-                     :assoc-in
-                     :dissoc
-                     :merge
-                     :select-keys
-                     :update-in]}
+                      #'get
+                      #'get-in
+                      #'contains?
+                      #'keys
+                      #'vals]
+           "change" [#'assoc
+                     #'assoc-in
+                     #'dissoc
+                     #'merge
+                     #'select-keys
+                     #'update-in]}
 
    "sets" {"create" ["#{}"
-                     :set]
+                     #'set]
            "examine" ["(your-set k)"
-                      :get
-                      :contains?]
-           "change" [:conj
-                     :disj]
-           "ops"  [:clojure.set/union
-                   :clojure.set/difference
-                   :clojure.set/intersection]
-           "test" [:clojure.set/subset?
-                   :clojure.set/superset?]}
+                      #'get
+                      #'contains?]
+           "change" [#'conj
+                     #'disj]
+           "ops"  [#'clojure.set/union
+                   #'clojure.set/difference
+                   #'clojure.set/intersection]
+           "test" [#'clojure.set/subset?
+                   #'clojure.set/superset?]}
 
-   "atoms" {"create" [:atom]
-            "change" [:swap! :reset!]
-            "read" ["@" :deref]}})
+   "atoms" {"create" [#'atom]
+            "change" [#'swap! #'reset!]
+            "read" ["@" #'deref]}})
