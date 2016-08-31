fae5e4b95fb770a85c4d91ad2c6227d582942390
Rafal Dittwald <rafal.dittwald@gmail.com>
2016-08-30 21:37:53 -0400

    Update content

--- src/cheatsheet/data.cljs
+++ src/cheatsheet/data.cljs
@@ -2,24 +2,19 @@
   (:require [clojure.pprint]))
 
 (def data
-  {"misc" {"comments" ["#_(...)" "comment" "; ..."]
-           "define" ["def" "defonce" "defn"]
-           "let" ["let" "if-let" "when-let"]
-           "arrange" ["->" "->>"]
-           "namespace" ["ns"]
-           "function" ["fn"]}
+  {; BASICS
 
-   "branching" {"logic" ["if" "when" "if-not" "when-not" "cond" "case" "condp"]
-                "predicates" [#'= #'not=]
-                "operators" ["and" "or" #'not]}
+   "basics" {"define" ["def" "defonce" "defn" "let" "declare" "ns"]
+             "branch" ["if" "when" "if-not" "when-not" "cond" "case" "condp" "if-let" "when-let"]
+             "compare" [#'= #'not= "and" "or" #'not]
+             "comments" ["#_(...)" "comment" "; ..."]}
 
+   "functions" {"create" ["fn" "defn" #'identity #'constantly #'comp #'partial]
+                "arrange" ["->" "->>"]}
 
-   "literals" {"create" ["true" "false" "nil"]
-               "test " [#'true? #'false? #'nil?]}
-
-   "numbers" {"create"     ["1" "-1.5" #'int #'long]
+   "numbers" {"literals"   ["1" "-1.5"]
               "arithmetic" [#'+ #'- #'* #'/ #'max #'min #'inc #'dec]
-              "compare"    [#'== #'< #'> #'<= #'>=]
+              "compare"    [#'== #'not= #'< #'> #'<= #'>=]
               "random"     [#'rand #'rand-int]}
 
    "strings" {"create"     ["\"some-text\"" #'str]
@@ -27,35 +22,48 @@
                             #'clojure.string/join
                             #'clojure.string/split
                             #'clojure.string/replace]
-              "test"       [#'clojure.string/blank?
-                            #'clojure.string/includes?]
-              "regex"      ["#\"some-patern\""
+              "letters"    [#'clojure.string/upper-case
+                            #'clojure.string/lower-case
+                            #'clojure.string/capitalize]
+              "regex"      ["#\"some-pattern\""
                             #'re-find
-                            #'re-matches]}
+                            #'re-matches
+                            #'re-pattern
+                            #'clojure.string/replace]
+              "test"       [#'clojure.string/blank?
+                            #'clojure.string/includes?]}
 
-   "keywords" {"create"    [":some-keyword"
-                            ":some-ns/some-keyword"
-                            #'keyword]
-               "->str"     [#'name
-                            #'namespace]}
+   "atoms" {"create" [#'atom]
+            "change" [#'swap! #'reset!]
+            "read" ["@" #'deref]}
+
+   "js interop" {"create" ["#js { }" "#js [ ]"]
+                 "shorthand" ["." ".." "doto"]
+                 "properties" ["aget" "set" "aset"]
+                 "convert" ["cljs->js" "js->clj"]}
+
+   ; COLLECTIONS
 
    "sequences" {"misc" [#'map
+                        #'map-indexed
                         #'reduce
                         #'apply
-                        "for"
-                        "doseq"
-                        #'concat
                         #'count
                         #'some
-                        ]
+                        #'group-by]
+                "create" [#'repeat
+                          #'range]
                 "loop" ["for" "doseq"]
+                "longer" [#'conj
+                          #'concat]
                 "test" [#'some?
                         #'empty?
                         #'every?]
                 "shorter" [#'filter
                            #'remove
                            #'rest
-                           #'drop]
+                           #'drop
+                           #'keep]
                 "reorder" [#'shuffle
                            #'reverse
                            #'sort
@@ -68,6 +76,7 @@
    "vectors" {"create" ["[]"
                         #'vec]
               "examine" ["(your-vec idx)"
+                         #'nth
                          #'get]
               "change" [#'assoc
                         #'assoc-in
@@ -101,15 +110,19 @@
            "test" [#'clojure.set/subset?
                    #'clojure.set/superset?]}
 
-   "atoms" {"create" [#'atom]
-            "change" [#'swap! #'reset!]
-            "read" ["@" #'deref]}
+; OTHER
+
+"literals" {"create" ["true" "false" "nil"]
+            "test " [#'true? #'false? #'nil?]}
+
+"keywords" {"create"    [":some-keyword"
+                         ":some-ns/some-keyword"
+                         #'keyword]
+            "->str"     [#'name
+                         #'namespace]}
 
 "io" {"files" ["spit"
                "slurp"]
       "*out*" [#'println #'clojure.pprint/pprint]}
-
-"js interop" ["." ".." "cljs->js" "js->clj" "doto"
-              "#js { }"]
 
 "repl" ["*1" "clojure.repl/pst"]})
