de702a2c827a4656bfd1b3bbc3d7fc7a2e67d5aa
Rafal Dittwald <rafal.dittwald@gmail.com>
2019-10-23 19:45:52 -0400

    Update dependencies

--- project.clj
+++ project.clj
@@ -1,12 +1,12 @@
 (defproject cognitory.cheatsheet "0.0.1"
 
-  :dependencies [[org.clojure/clojure "1.8.0"]
-                 [org.clojure/clojurescript "1.9.227"]
-                 [garden "1.3.2"]
-                 [reagent "0.6.0-rc"]]
+  :dependencies [[org.clojure/clojure "1.10.0"]
+                 [org.clojure/clojurescript "1.10.520"]
+                 [garden "1.3.9"]
+                 [reagent "0.8.1"]]
 
-  :plugins [[lein-figwheel "0.5.6"]
-            [lein-cljsbuild "1.1.4"]]
+  :plugins [[lein-figwheel "0.5.19"]
+            [lein-cljsbuild "1.1.7"]]
 
   :figwheel {:server-port 3453}
 
